6e5f1b5fed8cd213d1c0b3bdc1e9eb19efc3c40f
Muzaffar <qwertymuzaffar@gmail.com>
2025-10-27 11:14:27 +0500

    Updated SkeletonLoader.tsx

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -30,19 +30,19 @@ export function SkeletonLoader() {
                   <div className="h-4 bg-gray-200 rounded w-24"></div>
                 </th>
                 <th className="px-6 py-4">
-                  <div className="h-4 bg-gray-200 rounded w-28"></div>
+                  <div className="h-4 bg-gray-200 rounded w-24"></div>
                 </th>
                 <th className="px-6 py-4">
-                  <div className="h-4 bg-gray-200 rounded w-32"></div>
-                </th>
-                <th className="px-6 py-4">
-                  <div className="h-4 bg-gray-200 rounded w-32"></div>
+                  <div className="h-4 bg-gray-200 rounded w-36"></div>
                 </th>
                 <th className="px-6 py-4">
                   <div className="h-4 bg-gray-200 rounded w-24"></div>
                 </th>
                 <th className="px-6 py-4">
-                  <div className="h-4 bg-gray-200 rounded w-20"></div>
+                  <div className="h-4 bg-gray-200 rounded w-24"></div>
+                </th>
+                <th className="px-6 py-4">
+                  <div className="h-4 bg-gray-200 rounded w-24"></div>
                 </th>
               </tr>
             </thead>
@@ -53,22 +53,22 @@ export function SkeletonLoader() {
                     <div className="h-4 bg-gray-200 rounded w-4"></div>
                   </td>
                   <td className="px-6 py-4">
-                    <div className="h-4 bg-gray-200 rounded w-28"></div>
+                    <div className="h-4 bg-gray-200 rounded w-24"></div>
                   </td>
                   <td className="px-6 py-4">
-                    <div className="h-4 bg-gray-200 rounded w-32"></div>
+                    <div className="h-4 bg-gray-200 rounded w-24"></div>
                   </td>
                   <td className="px-6 py-4">
-                    <div className="h-4 bg-gray-200 rounded w-32"></div>
+                    <div className="h-4 bg-gray-200 rounded w-36"></div>
                   </td>
                   <td className="px-6 py-4">
-                    <div className="h-5 bg-gray-200 rounded-full w-28"></div>
+                    <div className="h-4 bg-gray-200 rounded w-24"></div>
                   </td>
                   <td className="px-6 py-4">
-                    <div className="h-4 bg-gray-200 rounded w-20"></div>
+                    <div className="h-4 bg-gray-200 rounded w-24"></div>
                   </td>
                   <td className="px-6 py-4">
-                    <div className="h-8 bg-gray-200 rounded w-16"></div>
+                    <div className="h-4 bg-gray-200 rounded w-24"></div>
                   </td>
                 </tr>
               ))}
