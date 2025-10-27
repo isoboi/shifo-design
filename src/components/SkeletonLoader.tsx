509f981f568a74b616c04c86c98ba93528f851ac
Muzaffar <qwertymuzaffar@gmail.com>
2025-10-27 11:12:56 +0500

    Updated SkeletonLoader.tsx

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -68,10 +68,7 @@ export function SkeletonLoader() {
                     <div className="h-4 bg-gray-200 rounded w-20"></div>
                   </td>
                   <td className="px-6 py-4">
-                    <div className="flex space-x-2">
-                      <div className="h-8 bg-gray-200 rounded w-8"></div>
-                      <div className="h-8 bg-gray-200 rounded w-8"></div>
-                    </div>
+                    <div className="h-8 bg-gray-200 rounded w-16"></div>
                   </td>
                 </tr>
               ))}
